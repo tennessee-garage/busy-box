55263b50d9b778bb8e9c1b2c346b97c22e025f97
Garth Webb <garth.webb@gmail.com>
2024-07-09 19:48:25 -0700

    Fixed SPI functionality for 3+ client devices. This included removing the interrupt functionality, as that was creating too many timing issues.  Additionally, the DO line is pulsed as a way to alert the host that the client is ready, in an attempt to reduce the time the host has to "wait and hope" that the client is ready after bringing the select line low.  Finally, another big issue was making sure that the DO line was kept as an input at all times when its not in use.

--- Busy Controls/lib/TinySPI/TinySPI.cpp
+++ Busy Controls/lib/TinySPI/TinySPI.cpp
@@ -23,63 +23,19 @@ TinySPI::TinySPI(uint8_t do_pin, uint8_t di_pin, uint8_t clk_pin, uint8_t en_pin
     _di_pin = di_pin;
     _clk_pin = clk_pin;
     _enable_pin = en_pin;
+    _response_byte = 0;
 
-    // Set the data out pin to INPUT to keep it floating until we're actually selected.
+    // We start DO off as an input and set to LOW, i.e. floating.  We only make
+    // it an output when we're selected.  If we don't do this, at 3+ clients on
+    // the bus, it becomes impossible for any one client to successfully set
+    // this line. as the others are all pulling it down.
     pinMode(_do_pin, INPUT);
 
     pinMode(_di_pin, INPUT);
     pinMode(_enable_pin, INPUT_PULLUP);
     pinMode(_clk_pin, INPUT);
-}
 
-void TinySPI::begin() {
-    // Enable pin change interrupt on tne enable pin
-    GIMSK |= (1 << PCIE0);
-
-    // Enable appropriate interrupt for the given select pin 
-    switch (_enable_pin) {
-        case PIN_A0:
-            PCMSK0 |= (1 << PCINT0);
-            break;
-        case PIN_A1:
-            PCMSK0 |= (1 << PCINT1);
-            break;
-        case PIN_A2:
-            PCMSK0 |= (1 << PCINT2);
-            break;
-        case PIN_A3:
-            PCMSK0 |= (1 << PCINT3);
-            break;
-        case PIN_A4:
-            PCMSK0 |= (1 << PCINT4);
-            break;
-        case PIN_A5:
-            PCMSK0 |= (1 << PCINT5);
-            break;
-        case PIN_A6:
-            PCMSK0 |= (1 << PCINT6);
-            break;
-        case PIN_A7:
-            PCMSK0 |= (1 << PCINT7);
-            break;
-        default:
-            break;
-    }
-
-    instance = this;
-
-    // Enable global interrupts
-    sei();
-}
-
-ISR(PCINT0_vect) {
-    if (TinySPI::instance == nullptr) {
-        return;
-    }
-
-    if (TinySPI::instance->is_chip_selected()) {
-        TinySPI::instance->enable_spi();
-    }
+    digitalWrite(_do_pin, LOW);
 }
 
 bool TinySPI::is_chip_selected() {
@@ -87,35 +43,54 @@ bool TinySPI::is_chip_selected() {
 }
 
 void TinySPI::enable_spi() {
-        pinMode(_do_pin, OUTPUT);
-        SPI_ON();
+    // Re-establish DO as an output
+    pinMode(_do_pin, OUTPUT);
 
-        // Clear overflow when we see the falling edge of the enable pin
-        CLEAR_OVERFLOW();
+    // Pulse the DO pin to let the host know we're ready.  Without this the 
+    // host needs to wait an arbitrary amount of time hoping this device will be
+    // ready by then.
+    digitalWrite(_do_pin, LOW);
+    digitalWrite(_do_pin, HIGH);
+    digitalWrite(_do_pin, LOW);
 
-        // Load whatever our current response byte is
-        USIDR = _response_byte;
+    SPI_ON();
+
+    // Clear overflow when we see the falling edge of the enable pin
+    CLEAR_OVERFLOW();
+
+    // Load whatever our current response byte is
+    USIDR = _response_byte;
 }
 
 void TinySPI::disable_spi() {
-    pinMode(_do_pin, INPUT);
     SPI_OFF();
+
+    // Make sure DO is set floating, so other clients can use this shared line
+    pinMode(_do_pin, INPUT);
+    digitalWrite(_do_pin, LOW);
 }
 
 bool TinySPI::poll_byte() {
+    bool has_received_data = false;
+
     if (!is_chip_selected()) {
-        return false;
+        return has_received_data;
     }
 
-    // Wait for bytes to be read in, at which point overflow will trigger
-    while (NOT_IN_OVERFLOW());
+    enable_spi();
 
-    // Read the value we got in and immediately write out the value we want to send forward
-    _last_byte = USIDR;
+    // Wait for bytes to be read in, at which point overflow will trigger
+    while (NOT_IN_OVERFLOW() && is_chip_selected());
+
+    if (IN_OVERFLOW()) {
+        // Read the value we got in and immediately write out the value we want to send forward
+        _last_byte = USIDR;
+        has_received_data = true;
+    }
 
     disable_spi();
 
-    return true;
+    return has_received_data;
 }
 
 // This value gets shifted off as the response while data from the master is comming in,
